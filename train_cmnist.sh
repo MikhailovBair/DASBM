1e2ebcbb95ebc82bddc75eacd0e39c3d76795be4
Ivan Razvorotnev <irazvor@gmail.com>
2024-10-26 19:05:12 +0300

    Fixes

--- train_cmnist.sh
+++
@@ -1,8 +0,0 @@
-current_T=4
-current_innter_proj_iters=25000
-current_proj_iters=50000
-cur_ema_decay=0.999
-
-papermill MNIST_ASBM_IPMF.ipynb -p 
-
-#papermill MNIST_ASBM_IPMF.ipynb -p plan $2 -p T $current_T -p eps $1 -p markovian_proj_iters $current_proj_iters -p inner_ipmf_mark_proj_iters $current_innter_proj_iters -p ema_decay $cur_ema_decay  MNIST_ASBM_IPMF_log.ipynb 
